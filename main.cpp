16e2de4c58353b056195463570414b5286b59813
solfxck <gleb.naymof@gmail.com>
2024-03-24 16:25:26 +0700

    Прямоугольник v2

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 void calculRectangle(double side1, double side2) {
-    double perimetr = (side1 + side2) * 2;
-    double area = side1 * side2;
-    double diagonal = sqrt(side1 * side1 + side2 * side2);
+    double perimetr = (side1 + side2) * 2; //Периметр
+    double area = side1 * side2; //Площадь 
+    double diagonal = sqrt(side1 * side1 + side2 * side2); //Длина диагонали
+    
     cout << "Perimetr: " << perimetr << endl;
     cout << "Area: " << area << endl;
     cout << "Diagonal: " << diagonal << endl;
 }
 
 int main(){
-    calculRectangle(5.0, 6.0);
+    double side1, side2;
+    cout << "a. Rectangle\nEnter two values: ";
+    cin >> side1 >> side2;
+    calculRectangle(side1, side2);
 }
